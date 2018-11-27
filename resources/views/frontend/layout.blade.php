7af43c22367d97f496195f538af883d5f2969fef
user fbtwitter <rzaugusdi354@gmail.com>
2018-11-28 00:25:39 +0700

    baru home page..

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -164,47 +164,46 @@
 
     @yield('body-content')
 
-    <footer id="contact " class="footer " style="background-image: url( '{{ asset('front/assets/img/bg-footer.jpg
-                            ') }}') ">
-        <div class="container text-center ">
-            <div class="row ">
-                <div class="col-md-4 contact-details ">
-                    <h4><i class="fa fa-phone "></i> Call</h4>
+    <footer id="contact" class="footer" style="background-image: url('{{ asset('front/assets/img/bg-footer.jpg') }}')">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-4 contact-details">
+                    <h4><i class="fa fa-phone"></i> Call</h4>
                     <p>555-213-4567</p>
                 </div>
-                <div class="col-md-4 contact-details ">
-                    <h4><i class="fa fa-map-marker "></i> Visit</h4>
+                <div class="col-md-4 contact-details">
+                    <h4><i class="fa fa-map-marker"></i> Visit</h4>
                     <p>3481 Melrose Place
                         <br>Beverly Hills, CA 90210</p>
                 </div>
-                <div class="col-md-4 contact-details ">
-                    <h4><i class="fa fa-envelope "></i> Email</h4>
-                    <p><a href="mailto:mail@example.com ">mail@example.com</a>
+                <div class="col-md-4 contact-details">
+                    <h4><i class="fa fa-envelope"></i> Email</h4>
+                    <p><a href="mailto:mail@example.com">mail@example.com</a>
                     </p>
                 </div>
             </div>
-            <div class="row social ">
-                <div class="col-lg-12 ">
-                    <ul class="list-inline ">
-                        <li><a href="# "><i class="fa fa-facebook fa-fw fa-2x "></i></a>
+            <div class="row social">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li><a href="#"><i class="fa fa-facebook fa-fw fa-2x"></i></a>
                         </li>
-                        <li><a href="# "><i class="fa fa-twitter fa-fw fa-2x "></i></a>
+                        <li><a href="#"><i class="fa fa-twitter fa-fw fa-2x"></i></a>
                         </li>
-                        <li><a href="# "><i class="fa fa-linkedin fa-fw fa-2x "></i></a>
+                        <li><a href="#"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="row copyright ">
-                <div class="col-lg-12 ">
-                    <p class="small ">&copy; 2015 Start Bootstrap Themes</p>
+            <div class="row copyright">
+                <div class="col-lg-12">
+                    <p class="small">&copy; 2015 Start Bootstrap Themes</p>
                 </div>
             </div>
         </div>
     </footer>
 
     <!-- Core Scripts -->
-    <script src="{{ asset( 'front/assets/js/jquery.js') }} "></script>
+    <script src="{{ asset( 'front/assets/js/jquery.js') }}"></script>
     <script src="{{ asset( 'front/assets/js/bootstrap/bootstrap.min.js') }} "></script>
     <!-- Plugin Scripts -->
     <script src="{{ asset( 'front/assets/js/plugins/jquery.easing.min.js') }} "></script>
